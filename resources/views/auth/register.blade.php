d58ff0ca8fea88246a4b94e1a980fdb4334aafbe
Osa Hady <osahady@gmail.com>
2019-09-01 15:52:25 +0300

    registration form validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,20 +7,43 @@
     <div>
         <label for="name">Name</label>
         <input type="text" name="name" value="{{old('name')}} " 
-        class="form-control" required>
+        class="form-control {{$errors->has('name') ? 'is-invalid' : ''}} " required>
+       
+        @if ($errors->has('name'))
+            <span class="invalid-feedback">
+                <strong>{{$errors->first('name')}} </strong>
+            </span>    
+        @endif
+
     </div>
     <div>
         <label for="email">Email</label>
         <input type="text" name="email" value="{{old('email')}} " 
-        class="form-control" required>
+        class="form-control {{$errors->has('email') ? 'is-invalid' : ''}} " required>
+        
+        @if ($errors->has('email'))
+            <span class="invalid-feedback">
+                <strong>{{$errors->first('email')}} </strong>
+            </span>    
+        @endif
+
     </div>
     <div>
         <label for="password">Password</label>
-        <input type="text" name="password" class="form-control" required>
+        <input type="password" name="password" 
+        class="form-control {{$errors->has('password') ? 'is-invalid' : ''}} " required>
+        
+        @if ($errors->has('password'))
+            <span class="invalid-feedback">
+                <strong>{{$errors->first('password')}} </strong>
+            </span>    
+        @endif
+
     </div>
     <div>
         <label for="password_confirmation">Retype</label>
-        <input type="text" name="password_confirmation" class="form-control" required>
+        <input type="password" name="password_confirmation" class="form-control" required>
+      
     </div>
     <button class="btn btn-primary btn-block" type="submit">Register</button>
 </form>
--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,3 @@ Route::get('/contact', 'HomeController@contact')->name('contact');
 Route::resource('/posts', 'PostController'); 
 Auth::routes();
 
-
-Route::get('/home', 'HomeController@index')->name('home');
