2b7732273fc6dd251129d284011f1b2208ef5137
Osa Hady <osahady@gmail.com>
2019-09-02 16:19:00 +0300

    protecting routes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,12 @@ use App\BlogPost;
 use App\Http\Requests\StorePost;
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->only([
+            'create', 'edit', 'destroy','store', 'update'
+        ]);
+    }
     /**
      * Display a listing of the resource.
      *
