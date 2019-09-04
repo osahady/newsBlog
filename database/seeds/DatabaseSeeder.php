3c623c772624379ab20b3ac0cec3a897cb94707e
Osa Hady <osahady@gmail.com>
2019-09-04 21:55:41 +0300

    factory user to add 20 user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,8 +20,8 @@ class DatabaseSeeder extends Seeder
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
-            ]
-        );
+        ]);
+        factory(User::class, 20)->create();
 
     }
 }
