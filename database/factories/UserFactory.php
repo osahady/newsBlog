7551116b114d91e59518f4a6b9a2eff1461f26b4
Osa Hady <osahady@gmail.com>
2019-09-04 22:26:32 +0300

    seeding with a states user method

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ use App\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
+
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -23,5 +24,12 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->state(User::class, 'osa-hady', function (Faker $faker){
+    return [
+        'name' => 'Osa Hady',
+        'email' => 'osahady@gmail.com',
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,13 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        DB::table('users')->insert([
-                'name' => 'Osa Hady',
-                'email' => 'osahady@gmail.com',
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10),
-        ]);
+        factory(User::class)->states('osa-hady')->create();
         factory(User::class, 20)->create();
 
     }
