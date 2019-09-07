9874ae3b4fd9f63f14219ad853a9631b3c0ef45a
Osa Hady <osahady@gmail.com>
2019-09-07 15:06:19 +0300

    softDeletes

--- app/BlogPost.php
+++ app/BlogPost.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
+    use SoftDeletes;
     protected $fillable = ['title', 'content'];  
     //protected $guarded = [];
     public function comments()
