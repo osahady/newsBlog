46f951023e309e299c1c04541b2d2eb1d0b644d7
Osa Hady <osahady@gmail.com>
2019-09-08 10:02:52 +0300

    comments also soft deleted

--- app/Comment.php
+++ app/Comment.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\BlogPost;
 class Comment extends Model
 {
+    //use trait
+    use SoftDeletes;
+
     protected $guarded = [];
     public function blogPost()
     {
